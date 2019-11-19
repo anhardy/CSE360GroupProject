f14af6a64d474bb5f09ef1b9bcf3d06909239db3
ahard <ahard@DESKTOP-BSQVR7T.3521.dhcp.asu.edu>
2019-11-19 14:10:36 -0700

    Basic GUI Functionality added. Load and Save open file browsers, and exit closes the program. Non-functional load and save methods added.

--- src/lineformatter/LineFormatter.java
+++ src/lineformatter/LineFormatter.java
@@ -4,18 +4,20 @@
  * and open the template in the editor.
  */
 package lineformatter;
+import java.io.File;
 
 /**
  *
  * @author ahard
  */
 public class LineFormatter {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    File load (File file) {
+        return file;
     }
+    void save (File file) {
+        
+    }
+
+   
     
 }
