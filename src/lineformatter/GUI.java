5342cd4fded882f3c500d8f7616719ca58116bd8
ahard <ahard@DESKTOP-BSQVR7T.3521.dhcp.asu.edu>
2019-12-02 20:16:51 -0700

    GUI renamed.

---
+++ src/lineformatter/GUI.java
@@ -0,0 +1,215 @@
+package lineformatter;
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * This class is a simple GUI for loading files for formatting, then saving the
+ * formatted file.
+ * @author ahard
+ */
+public class GUI extends javax.swing.JFrame {
+    private LineFormatter formatter = new LineFormatter();
+
+    /**
+     * Creates new form GUIPrototype
+     */
+    public GUI() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jDisplayLabel = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jDisplay = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jErrors = new javax.swing.JTextArea();
+        jErrorLabel = new javax.swing.JLabel();
+        jButtonLoad = new javax.swing.JButton();
+        jButtonSave = new javax.swing.JButton();
+        jButtonExit = new javax.swing.JButton();
+
+        jLabel1.setText("jLabel1");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jDisplayLabel.setText("Display:");
+
+        jDisplay.setColumns(20);
+        jDisplay.setRows(5);
+        jScrollPane2.setViewportView(jDisplay);
+
+        jErrors.setColumns(20);
+        jErrors.setRows(5);
+        jScrollPane3.setViewportView(jErrors);
+
+        jErrorLabel.setText("Errors:");
+
+        jButtonLoad.setText("Load");
+        jButtonLoad.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonLoadMouseClicked(evt);
+            }
+        });
+        jButtonLoad.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonLoadActionPerformed(evt);
+            }
+        });
+
+        jButtonSave.setText("Save");
+        jButtonSave.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonSaveMouseClicked(evt);
+            }
+        });
+
+        jButtonExit.setBackground(new java.awt.Color(255, 51, 51));
+        jButtonExit.setText("Exit");
+        jButtonExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonExitMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButtonExit)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButtonLoad)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonSave))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jErrorLabel)
+                            .addComponent(jDisplayLabel))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1204, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jDisplayLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 385, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(jErrorLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonLoad)
+                    .addComponent(jButtonSave)
+                    .addComponent(jButtonExit))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonLoadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLoadActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonLoadActionPerformed
+
+    private void jButtonLoadMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonLoadMouseClicked
+        JFileChooser fileChooser = new JFileChooser();
+        int response = fileChooser.showOpenDialog(null);
+        if(response == JFileChooser.APPROVE_OPTION) {
+            formatter.clearErrors();
+            formatter.clearFormattedLines();
+            jDisplay.setText("");
+            jErrors.setText("");
+            ArrayList<String> errors = new ArrayList<>();
+            ArrayList<String> formattedLines = new ArrayList<>();
+            File loadedFile = fileChooser.getSelectedFile();
+            formatter.format(loadedFile);
+            errors = formatter.getErrors();
+            formattedLines = formatter.getFormattedLines();
+            for(int i = 0; i < errors.size(); i++) {
+                jErrors.append(errors.get(i));
+                jErrors.append("\n");
+            }
+             for(int i = 0; i < formattedLines.size(); i++) {
+                jDisplay.append(formattedLines.get(i));
+            }
+        }
+    }//GEN-LAST:event_jButtonLoadMouseClicked
+
+    private void jButtonSaveMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonSaveMouseClicked
+         JFileChooser fileChooser = new JFileChooser();
+        int response = fileChooser.showSaveDialog(null);
+        if(response == JFileChooser.APPROVE_OPTION) {
+            File savedFile = fileChooser.getSelectedFile();
+            formatter.save(savedFile);
+        }
+    }//GEN-LAST:event_jButtonSaveMouseClicked
+
+    private void jButtonExitMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonExitMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_jButtonExitMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonExit;
+    private javax.swing.JButton jButtonLoad;
+    private javax.swing.JButton jButtonSave;
+    protected javax.swing.JTextArea jDisplay;
+    private javax.swing.JLabel jDisplayLabel;
+    private javax.swing.JLabel jErrorLabel;
+    protected javax.swing.JTextArea jErrors;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextArea jTextArea1;
+    // End of variables declaration//GEN-END:variables
+}
